fc508edb6d65ab5e0631b9da8942917337ade604
D-cun0205 <mmeo0205@gmail.com>
2022-09-13 18:36:15 +0900

    react-todo

--- src/App.js
+++ src/App.js
@@ -1,119 +1,15 @@
-import "./App.css";
-import React, { useReducer, useCallback, useRef, useMemo } from "react";
-import UserList from "./UserList";
-import CreateUser from "./CreateUser";
-import useInputs from "./hooks/useInputs";
-
-function countActiveUser(users) {
-  return users.filter(user => user.active).length;
-}
-
-const initialState = {
-  inputs: {
-    username: "",
-    email: ""
-  },
-  users: [
-    {
-      id: 1,
-      username: "sanghun",
-      email: "public.sanghun@email.com",
-      active: true
-    },
-    {
-      id: 2,
-      username: "dcun",
-      email: "public.dcun@email.com",
-      active: false
-    },
-    {
-      id: 3,
-      username: "test",
-      email: "public.test@email.com",
-      active: false
-    }
-  ]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "CHANGE_INPUT":
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
-    case "CREATE_USER":
-      return {
-        users: state.users.concat(action.user)
-      };
-    case "TOGGLE_USER":
-      return {
-        users: state.users.map(user =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        )
-      };
-    case "REMOVE_USER":
-      return {
-        users: state.users.filter(user => user.id !== action.id)
-      };
-    default:
-      return state;
-  }
-}
+import React from "react";
+import "./App.scss";
+import Button from "./components/Button";
 
 function App() {
-  const [{ username, email }, onChange, reset] = useInputs({
-    username: "",
-    email: ""
-  });
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
-
-  const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: "CREATE_USER",
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    reset();
-    nextId.current += 1;
-  }, [username, email, reset]);
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: "TOGGLE_USER",
-      id
-    });
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: "REMOVE_USER",
-      id
-    });
-  }, []);
-
-  const count = useMemo(() => countActiveUser(users), [users]);
 
   return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
-      <div>활성사용자 수 : {count}</div>
-    </>
+    <div className="App">
+      <div className="buttons">
+        <Button>Button</Button>
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import Counter from "./Counter";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
